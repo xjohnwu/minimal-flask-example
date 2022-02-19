24f161679f521eb1dace5b6520865a9ae9a6cadd
Han Wu <xjohnwu@gmail.com>
2022-02-19 09:56:00 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     ####################
     # FOR DEVELOPMENT
     ####################
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='127.0.0.1', debug=True)
 
